a4205c1cfe338a4ed841d2104ed5a5c2cc31092a
Mikella Seth <mikellaseth@Mikellas-MacBook-Pro.local>
2016-10-26 20:23:39 +0800

    Post capabilities improved to allow random coloring and appending and mobile responsivity. Still have bugs via post appendage when routing between pages

--- app/assets/javascripts/channels/post.js
+++ app/assets/javascripts/channels/post.js
@@ -2,15 +2,35 @@ $(document).on('turbolinks:load', function () {
   connectToChat()
 })
 
+var cardColors =
+[
+  "rgb(105, 170, 182)",
+  "rgb(209, 55, 102)",
+  "rgb(99, 82, 171)",
+  "rgb(234, 212, 70)",
+  "rgb(59, 131, 149)",
+  "rgb(216, 89, 133)"
+]
+
 function connectToChat () {
   if (typeof current_room_id !== 'undefined') {
     App.posts = App.cable.subscriptions.create({ channel: 'PostsChannel', room: current_room_id }, {
       received: function (data) {
         console.log('ActionCable posts data recieved on client:', data)
-
+        var randomPicker = Math.floor(Math.floor(Math.random() * (cardColors.length)))
         $('#posts').removeClass('hidden')
-
-        $('#posts').append('<div class="card"> <p> <b>' + data.user + ': </b>' + data.message + '</p> </div>')
+        console.log("POST JS IS THE CULPRIT")
+        $('#posts').append('<div class="card" style="background-color:'+cardColors[randomPicker]+'"> <p> <b>' + data.user + ': </b>' + data.message + '</p> </div>')
+        var grid = new Minigrid({
+          container: '.cards',
+          item: '.card',
+          gutter: 6
+        });
+        grid.mount()
+        function update() {
+          grid.mount();
+        }
+        window.addEventListener('resize', update);
       },
       connected: function () {
         // Called when the subscription is ready for use on the server
--- app/assets/javascripts/pulsechannel.js
+++ app/assets/javascripts/pulsechannel.js
@@ -1,11 +1,23 @@
 $(document).on('turbolinks:load', function () {
   submitNewPost()
+  colorCards()
+
+  var grid = new Minigrid({
+    container: '.cards',
+    item: '.card',
+    gutter: 6
+  });
+  grid.mount()
+  function update() {
+    grid.mount();
+  }
+  window.addEventListener('resize', update);
+  console.log("PULSECHANNEL JS IS THE CULPRIT")
 })
 
 function submitNewPost () {
   $('#trythis').keydown(function (event) {
     if (event.keyCode == 13) {
-      console.log("WE GOT TO THE SUBMIT POST PART THIS IS THE EVENT: "+  event)
       // can send post to actioncable, as well as triggering from the model creation
       App.posts.send({ user: current_username, post: $('[data-textarea="post"]').val() })
       $('[data-send="post"]').click()
@@ -14,4 +26,25 @@ function submitNewPost () {
       return false
     }
   })
+}
+
+
+//Randomize card colors
+
+var cardColors =
+[
+  "rgb(105, 170, 182)",
+  "rgb(209, 55, 102)",
+  "rgb(99, 82, 171)",
+  "rgb(234, 212, 70)",
+  "rgb(59, 131, 149)",
+  "rgb(216, 89, 133)"
+]
+
+function colorCards(){
+  var cards = document.getElementsByClassName('card')
+  for(i=0;i<cards.length;i++){
+    var randomPicker = Math.floor(Math.floor(Math.random() * (cardColors.length)))
+    cards[i].style.backgroundColor = cardColors[randomPicker]
+  }
 }
