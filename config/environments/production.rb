b480dd3f7111a7184f574edf96d203841ac5c22d
Mikella Seth <mikellaseth@Mikellas-MacBook-Pro.local>
2016-10-28 13:41:13 +0800

    Config update 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,10 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = ['https://pulsechannel.herokuapp.com','http://pulsechannel.herokuapp.com']
-  config.action_cable.url = "wss://sitepoint-actioncable.herokuapp.com/cable"
+  config.action_cable.url = 'wss://sitepoint-actioncable.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://pulsechannel.herokuapp.com', /http:\/\/pulsechannel.herokuapp.*/ ]
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
