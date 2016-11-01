9c6c5bcbb197a5de1255ba88c1448ddc91d83dd1
Mikella Seth <mikellaseth@Mikellas-MacBook-Pro.local>
2016-11-01 10:42:09 +0800

    Fixed bugs for turbolinks

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,5 @@
-$(document).on('turbolinks:load', function () {
-console.log("turbolinks loaded on welcome page")
-});
 
 $(document).ready(function(){
-  function trythisone(){
-  }
   console.log("click functionality loaded")
   $('#create-channel-form').click(function(){
     $('#create-channel-dropdown').slideDown( "slow");
